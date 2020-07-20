032fa8cdeef959257fd34b5a0e17085f64c9e978
Dawid <dawid.badurakrk@gmail.com>
2020-07-20 13:40:11 +0200

    delete

--- src/main/java/com/crud/tasks/repository/TaskRepository.java
+++ src/main/java/com/crud/tasks/repository/TaskRepository.java
@@ -9,5 +9,10 @@ import java.util.Optional;
 public interface TaskRepository extends CrudRepository<Task, Long> {
     @Override
     List<Task> findAll();
+    @Override
     Optional<Task> findById(Long id);
+    @Override
+    Task save (Task task);
+    @Override
+    void deleteById(Long id);
 }
