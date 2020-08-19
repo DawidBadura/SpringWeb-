91ebbba58172efff18efc661d990dbcd16c822fa
Dawid <dawid.badurakrk@gmail.com>
2020-08-20 00:44:40 +0200

    sssc

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -1,6 +1,5 @@
 package com.crud.tasks;
 
-import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
