552a2f67e62c9a937865f9631b0302591b058849
Dawid <dawid.badurakrk@gmail.com>
2021-03-10 22:26:50 +0100

    deploy online

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -2,14 +2,16 @@ package com.crud.tasks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TasksApplication/* extends SpringBootServletInitializer*/  {
+public class TasksApplication extends SpringBootServletInitializer {
 
-/*	@Override
+	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(TasksApplication.class);
-	}*/
+	}
 
 
     public static void main(String[] args) {
