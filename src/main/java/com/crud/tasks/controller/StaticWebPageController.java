f6378c25c0b15a87c53bcb7342cb1ca745be0d0a
Dawid <dawid.badurakrk@gmail.com>
2021-03-16 22:11:27 +0100

    cross

--- src/main/java/com/crud/tasks/controller/StaticWebPageController.java
+++ src/main/java/com/crud/tasks/controller/StaticWebPageController.java
@@ -1,11 +1,14 @@
 package com.crud.tasks.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Map;
 
 @Controller
+@CrossOrigin("*")
+
 public class StaticWebPageController {
     @RequestMapping("/")
     public String index(Map<String, Object> model) {
